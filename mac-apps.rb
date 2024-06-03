8a0cbd83060b08c230f9fb6656dae8fed51321ef
JustasPolis <j.polikevicius@gmail.com>
2024-06-03 21:17:50 +0300

    1.3.0

--- mac-apps.rb
+++ mac-apps.rb
@@ -1,7 +1,7 @@
 class MacApps < Formula
   desc "CLI tool to query for macOS apps"
   homepage "https://github.com/JustasPolis/mac-apps-cli"
-  url "https://github.com/JustasPolis/mac-apps-cli/archive/refs/tags/1.2.0.tar.gz"
+  url "https://github.com/JustasPolis/mac-apps-cli/archive/refs/tags/1.3.0.tar.gz"
   sha256 "116f24b708477fe74365aa5054421351a5c5a723249edf16163fccb0433208d4"
   license "MIT"
 
