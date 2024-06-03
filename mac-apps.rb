6a5bd8a0d6664c3859d3e654b7406eb77364b0c5
JustasPolis <j.polikevicius@gmail.com>
2024-06-03 21:20:40 +0300

    sha

--- mac-apps.rb
+++ mac-apps.rb
@@ -2,7 +2,7 @@ class MacApps < Formula
   desc "CLI tool to query for macOS apps"
   homepage "https://github.com/JustasPolis/mac-apps-cli"
   url "https://github.com/JustasPolis/mac-apps-cli/archive/refs/tags/1.3.0.tar.gz"
-  sha256 "cc6b16898b81c3c9f5a29cc6a09d5b7cc23246d4f16f42e893a10b6810191000"
+  sha256 "116f24b708477fe74365aa5054421351a5c5a723249edf16163fccb0433208d4"
   license "MIT"
 
   depends_on xcode: [">= 15.2", :build]
